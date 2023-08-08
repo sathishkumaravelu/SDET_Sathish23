ce325900d94493026e805732287c8d5d02e5f30e
sathishkumaravelu <sathish@DESKTOP-B6NN9JJ>
2023-08-08 10:50:30 +0530

    Commit-> 08-08-2023

---
+++ src/main/java/mandatoryHomeWork/foundation/P60_threeDivisors.java
@@ -0,0 +1,50 @@
+package mandatoryHomeWork.foundation;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+/*
+ * 1. Assign a count variable count
+ * 2. Iterate thru the input 
+ * 3. increase count if the input divisible by iterator
+ * 4. finally check if the count is exactly 3, return true else false
+ * */
+
+public class P60_threeDivisors {
+	
+	@Test
+	public void testCase1() {
+		Assert.assertEquals(false, isThree(2));
+	}
+	@Test
+	public void testCase2() {
+		Assert.assertEquals(true, isThree(4));
+	}
+	
+	@Test
+	public void testCase3() {
+		Assert.assertEquals(false, isThree(5));
+	}
+	@Test
+	public void testCase4() {
+		Assert.assertEquals(false, isThree(6));
+	}
+
+	
+	public boolean isThree(int n) {
+		int count = 0;
+		int i = 1;
+		while (i <= n) {
+			if (n % i == 0) {
+				count++;
+			}
+			i++;
+		}
+		if (count == 3) {
+			return true;
+		}
+
+		return false;
+
+	}
+}
